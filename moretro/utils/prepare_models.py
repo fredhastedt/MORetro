51e3d8905ca9503e0d5fb9ef4201bed46eb057b7
Friedrich <friedrich.hastedt@gmail.com>
2025-09-11 17:55:56 +0200

    Update to preparation functions

--- moretro/utils/prepare_models.py
+++ moretro/utils/prepare_models.py
@@ -1,12 +1,18 @@
+import json
 import pickle
+import gin
 import pandas as pd
 import logging
-from moretro.inference.retro_prediction import OneStepModel, ConditionPrediction
+from pathlib import Path
+from typing import Callable
+
+from moretro.inference.retro_prediction import ConditionPrediction
+from moretro.inference.heuristic_functions import COST_MAPPING
 
 logger = logging.getLogger(__name__)
 
-
-def prepare_starting_mols(file_path: str) -> set[str]:
+@gin.configurable()
+def prepare_starting_mols(file_path: str | Path) -> set[str]:
     """
     Load building blocks from file
 
@@ -16,34 +22,32 @@ def prepare_starting_mols(file_path: str) -> set[str]:
     Returns:
         set[str]: Set of building blocks (SMILES
     """
-
-    if file_path.endswith(".csv"):
+    dir_path = Path(__file__).parent.parent
+    file_path = dir_path / Path(file_path)
+    if file_path.suffix == ".csv":
         starting_mol = set(pd.read_csv(file_path)["smiles"].tolist())
-    elif file_path.endswith(".pkl"):
+    elif file_path.suffix == ".pkl":
         with open(file_path, "rb") as f:
             starting_mol = pickle.load(f)
+    elif file_path.suffix == ".json":
+        with open(file_path, "r") as f:
+            starting_mol = set(json.load(f))
     else:
-        raise ValueError("Unsupported file format. Use .csv or .pkl")
+        raise ValueError("Unsupported file format. Use .csv or .pkl or .json")
 
     logger.info(f"Loaded {len(starting_mol)} building blocks from {file_path}")
     return starting_mol
 
-
-def prepare_retro_model(template_path: str, model_path: str) -> OneStepModel:
-    """
-    Loads the single-step retro model from path
-
-    Parameters:
-        template_path (str): Path to the template model file
-        model_path (str): Path to the trained model file
-
-    Returns:
-        TemplateModel: Instance of the retro prediction model
-    """
-    logging.info(f"Loading templates from {template_path}")
-    logging.info(f"Loading retro model from {model_path}")
-    one_step_model = OneStepModel(model_path, template_path)
-    return one_step_model
+@gin.configurable()
+def prepare_heuristic_fns(heuristics: list[str]) -> list[Callable[[str], float]]:
+    heuristic_fs = []
+    for heuristic in heuristics:
+        if heuristic in COST_MAPPING:
+            heuristic_fs.append(COST_MAPPING[heuristic])
+        else:
+            logger.error(f"Unknown heuristic: {heuristic}")
+            raise ValueError("Please ensure that heuristic is defined for all cost functions")
+    return heuristic_fs
 
 
 def prepare_condition_model():
