b93b597534ded77a58a40364aa142bec0f5f9d15
Friedrich <friedrich.hastedt@gmail.com>
2025-09-12 14:14:05 +0200

    MOSearch - improve weight handling and logging during search + bug fix np.min

--- moretro/search/mo_search.py
+++ moretro/search/mo_search.py
@@ -37,7 +37,7 @@ class MOSearch:
         time_budget: float = 0.0,
         weight_strategy: str = "it",  # "it" for iterative, "obj" for objective
     ):
-        self.max_depth = max_depth
+        self.max_depth = 2*max_depth
         self.retro_model = retro_model
         self.search_graph = MOGraph(
             target=target,
@@ -53,6 +53,7 @@ class MOSearch:
         self.weight_iter_budget = weight_iter_budget
         self.iteration_budget = iteration_budget
         self.time_budget = time_budget
+        self.weights_open: list[bool] = [True] * self.search_graph.no_weights
 
     def can_expand_retro(self, node: Nodes) -> bool:
         """
@@ -72,23 +73,37 @@ class MOSearch:
             isinstance(node, MolNode) and node.depth < self.max_depth and node.is_open
         )
 
-    def retro_expansion(self, nodes_and_weights: MolNodeAndWeights) -> None:
+    def retro_expansion(self, nodes_and_weights: MolNodeAndWeights) -> bool:
         """
         Expands node of graph by adding predictions of single-step model
         New nodes are added to the graph and values are updated accordingly
 
         Parameters:
             node (MolNode): The node to expand
+
+        Returns:
+            bool: True if early resampling is triggered (all weights blocked), False otherwise
         """
         nodes: list[MolNode] = []
-        for node, _ in nodes_and_weights:
-            if not self.can_expand_retro(node):
-                logger.error(
-                    f"Node {node} cannot be expanded in retrosynthetic direction. \n Aborting search..."
-                )
-                return
+        nodes_and_weights_copy = nodes_and_weights.copy()
+        for node, weight in nodes_and_weights_copy:
+            if not self.can_expand_retro(node) and node.depth >= self.max_depth:
+                for w in weight:
+                    if self.weights_open[w]:
+                        self.weights_open[w] = False
+                        logger.info(
+                            f"Node {node.smiles} cannot be expanded with depth {int(node.depth / 2)} and a pre-defined max depth {int(self.max_depth / 2)}."
+                        )
+                        logger.warning(f"Weight {w} is now blocked from expansion until resampling.")
+                nodes_and_weights.remove((node, weight))
+            elif not self.can_expand_retro(node):
+                logger.critical("Critical error in expansion logic. This is a bug.")
             else:
                 nodes.append(node)
+        
+        if not nodes:
+            logger.warning("All weights selected for expansion cannot expand further. Early resampling triggered.")
+            return True
 
         smiles = [node.smiles for node in nodes]
         predictions = self.retro_model.predict(
@@ -111,24 +126,36 @@ class MOSearch:
                 new_nodes_and_weights.union(nodes_and_weights)
             )
 
-    def spawn_new_weights(self, num_iter: int) -> None:
+        return False
+
+    def spawn_new_weights(self, num_iter: int, early_resampling: bool) -> int:
         """
         Samples new weights to screen the Pareto front
 
         Parameters:
             num_iter (int): The number of iterations to sample new weights
+            early_resampling (bool): Whether the resampling is triggered early due to all weights being blocked
+        
+        Returns:
+            int: 1 to reinit weight counter, 0 otherwise
         """
         if self.weight_strategy == "it":
-            if num_iter % self.weight_iter_budget == 0:
+            if num_iter == self.weight_iter_budget + 1 or early_resampling:
                 logger.info("Sampling new weights...")
+                if self.search_graph.weights_open.shape[0] < self.search_graph.no_weights:
+                    return -100 # exit search 
                 self.search_graph.reinitialize_graph()
+                return 1
         elif self.weight_strategy == "obj":
             logger.info("Sampling new weights as objectives are below threshold...")
-            pass  #! Yet to implement objective-based search
+            # TODO Yet to implement objective-based search
+            return 0
         else:
             logger.error(
                 f"Invalid weight strategy: {self.weight_strategy}. Use 'it' or 'obj'."
             )
+            return 0
+        return num_iter
 
     def choose_next_nodes(self) -> MolNodeAndWeights:
         """
@@ -138,18 +165,18 @@ class MOSearch:
             MolNodeAndWeights: A set of tuples containing nodes and their corresponding weight indices
         """
 
-        open_nodes = []
+        # Convert set to sorted list for consistent ordering
+        open_nodes = sorted(list(self.search_graph.open_nodes), key=lambda x: x.smiles)
         open_nodes_values = []
-        for node in self.search_graph.open_nodes:
-            open_nodes.append(node)
+        for node in open_nodes:
             open_nodes_values.append(node.total_value)
 
         open_values = np.array(open_nodes_values)  # dims are n_nodes x n_weights
-        max_values = np.max(open_values, axis=0)
-        is_max = open_values == max_values[None, :]
+        min_values = np.min(open_values, axis=0)
+        is_min = open_values == min_values[None, :]
         indices_per_dim = []
         for i in range(open_values.shape[1]):
-            dim_indices = np.where(is_max[:, i])[0].tolist()
+            dim_indices = np.where(is_min[:, i])[0].tolist()
             indices_per_dim.append(dim_indices)
 
         # Find which weight dimensions want to expand the same nodes
@@ -182,17 +209,28 @@ class MOSearch:
             4. Repeat until iteration budget is reached or Pareto front is stable
         """
         logger.info("Starting multi-objective search process...")
-        iter_counter = 0
+        iter_counter = 1
+        weight_iter = 1
         elapsed_time = 0
         start_time = time.time()
         while iter_counter < self.iteration_budget and elapsed_time < self.time_budget:
             torch.cuda.empty_cache()
-            self.spawn_new_weights(iter_counter)
-            if not self.search_graph.open_nodes:
-                logger.info("No more open nodes to expand. Search completed.")
+            weight_iter = self.spawn_new_weights(weight_iter, early_resampling=False)
+            break_condition = not self.search_graph.open_nodes or weight_iter < 0
+            if break_condition:
+                if not self.search_graph.open_nodes:
+                    logger.info("No open nodes left to expand.")
+                else:
+                    logger.info("All weights have been sampled, no more weights to explore.")
+                logger.info("Search process completed.")
                 break
 
             nodes_and_weights_to_expand = self.choose_next_nodes()
-            self.retro_expansion(nodes_and_weights_to_expand)
+
+            early_resampling = self.retro_expansion(nodes_and_weights_to_expand)
+            if early_resampling:
+                weight_iter = self.spawn_new_weights(iter_counter, early_resampling=True)
+                self.weights_open = [True] * self.search_graph.no_weights
             iter_counter += 1
+            weight_iter += 1
             elapsed_time = time.time() - start_time
