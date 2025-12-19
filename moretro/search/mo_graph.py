76f6d0c548e248286dfa1a2e6b254ec2428778d9
Friedrich <friedrich.hastedt@gmail.com>
2025-12-19 11:01:37 +0000

    bug fix mo_graph

--- moretro/search/mo_graph.py
+++ moretro/search/mo_graph.py
@@ -709,11 +709,16 @@ class MOGraph:
         # Filter points that sum to 1
         grid_weights = grid_points[np.isclose(grid_points.sum(axis=1), 1.0)]
         # spawn dummy weights s.t. len(weights) % no_weights == 0
+        # if bo selector, only retain weights with at least 0.5 convergence in last dimension
+        if self.bo_selector and not second_sample:
+            grid_weights = grid_weights[grid_weights[:, -1] >= 0.5]
+        random_weights = self.rng.dirichlet(np.ones(len(self.heuristic_fns)), size=20)
         i = 0
         while len(grid_weights) % self.no_weights != 0:
             grid_weights = np.vstack(
-                [grid_weights, grid_weights[i % len(grid_weights)]]
+                [grid_weights, random_weights[i]]
             )
+            i += 1
         logger.info(f"Generated {len(grid_weights)} grid-based weight vectors.")
         return grid_weights
 
